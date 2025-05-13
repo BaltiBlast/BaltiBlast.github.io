c2f40c2e1fded7793790e3705d23e33d96538162
BaltiBlast <fougeray.florian@gmail.com>
2025-05-13 16:43:01 +0200

    refactoring skills section

--- scripts/skills.methods.js
+++ scripts/skills.methods.js
@@ -3,6 +3,8 @@ const skillsMethods = {
     displaySkills();
   },
 
+  // ------------------------------------------------------------------------------------------------- //
+  // Display skills in the skills section ✅
   displaySkills: () => {
     const skillsSection = document.getElementById("skills-section");
 
@@ -12,37 +14,50 @@ const skillsMethods = {
       categoryContainer.classList.add("skills__category");
 
       // Category title
-      const skillsTitle = document.createElement("h3");
-      skillsTitle.classList.add("uppercase", "lato-bold", "secondary-color");
-      skillsTitle.innerText = skill.title;
+      const skillsTitle = createCategoryTitle(skill);
 
       // Skills container
-      const skillsContainer = document.createElement("div");
-      skillsContainer.classList.add("skills__list");
-
-      // Skills list
-      skill.listSkillsCategory.forEach((skillItem, index) => {
-        // Display each skill in a span
-        const skillSpan = document.createElement("span");
-        skillSpan.classList.add("skill-item", "lato-light");
-        skillSpan.innerText = skillItem;
-
-        // Add a comma between skills
-        if (index < skill.listSkillsCategory.length - 1) {
-          skillSpan.innerText += ", ";
-        }
-
-        skillsContainer.appendChild(skillSpan);
-      });
+      const skillsContainer = createSkillsList(skill);
 
       // Add title and skills container to the category container
-      categoryContainer.appendChild(skillsTitle);
-      categoryContainer.appendChild(skillsContainer);
+      categoryContainer.append(skillsTitle, skillsContainer);
 
       // Add categoryu container to the main section
       skillsSection.appendChild(categoryContainer);
     });
   },
+
+  // ------------------------------------------------------------------------------------------------- //
+  // Create category title ✅
+  createCategoryTitle: (skill) => {
+    const skillsTitle = document.createElement("h3");
+    skillsTitle.classList.add("uppercase", "lato-bold", "secondary-color");
+    skillsTitle.innerText = skill.title;
+    return skillsTitle;
+  },
+
+  // ------------------------------------------------------------------------------------------------- //
+  // Create skills list ✅
+  createSkillsList: (skill) => {
+    const skillsContainer = document.createElement("div");
+    skillsContainer.classList.add("skills__list");
+
+    // Skills list
+    skill.listSkillsCategory.forEach((skillItem, index) => {
+      // Display each skill in a span
+      const skillSpan = document.createElement("span");
+      skillSpan.classList.add("skill-item", "lato-light");
+      skillSpan.innerText = skillItem;
+
+      // Add a comma between skills
+      if (index < skill.listSkillsCategory.length - 1) {
+        skillSpan.innerText += ", ";
+      }
+
+      skillsContainer.appendChild(skillSpan);
+    });
+    return skillsContainer;
+  },
 };
 
-const { displaySkills } = skillsMethods;
+const { displaySkills, createCategoryTitle, createSkillsList } = skillsMethods;
